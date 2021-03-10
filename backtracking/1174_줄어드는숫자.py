39a465786401148ed6052136e9c59e4f5a2e671a
서강준 <kangjunlucky@gmail.com>
2021-03-10 23:15:09 +0900

    algo upload

---
+++ backtracking/1174_줄어드는숫자.py
@@ -0,0 +1,43 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+def perm(idx):
+    global cnt
+    global isFind
+    if isFind:
+        return
+    if idx == len(sel):
+        cnt += 1
+        if cnt == n:
+            ans.append(sel)
+            isFind = True
+        return
+    for j in range(10):
+        if isFind: break
+        if idx >= 1:
+            if j >= sel[idx-1]: continue
+            sel[idx] = j
+            perm(idx+1)
+        else:
+            if j == 0: continue
+            sel[idx] = j
+            perm(idx+1)
+
+
+if n <= 10:
+    print(n-1)
+else:
+    cnt = 10
+    i = 2
+    isFind = False
+    ans = []
+    while True:
+        if isFind: break
+        sel = [0 for _ in range(i)]
+        perm(0)
+        i += 1
+    for num in ans[0]:
+        print(num, end='')
