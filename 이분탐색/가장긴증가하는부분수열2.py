e97e0801007b0e26ccf744e9fa63445cc98750f8
DESKTOP-KVCQHCD\multicampus <kangjunlucky@gmail.com>
2021-08-04 23:43:17 +0900

    algo upload

--- 이분탐색/가장긴증가하는부분수열2.py
+++ 이분탐색/가장긴증가하는부분수열2.py
@@ -6,7 +6,7 @@ sequence = list(map(int,input().split()))
 # 하나씩 탐색하는데 크면 그냥 넣고 작으면 어디 위치와 교환시킬지 고른다.
 # 실제 수열과 LIS(Longest Increasing Sequence) 결과랑 값은 다르다 왜? 감소하더라도 교환해주기 때문!
 
-seq = [0] # 나중에 빼주면 된다. 넣고 시작하자
+seq = [0] # 나중에 빼주면 된다. 넣고 시작하자 
 
 for case in sequence:
     if seq[-1] < case:
