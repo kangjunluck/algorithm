2e0509272c69555b968f4ccbc7d20b28a7a1e835
DESKTOP-KVCQHCD\multicampus <kangjunlucky@gmail.com>
2021-08-06 14:01:37 +0900

    algo upload

---
+++ 이분탐색/용액.py
@@ -0,0 +1,21 @@
+n = int(input())
+data = list(map(int,input().split()))
+
+l = 0
+r = n-1
+answer = 2000000000
+ansl = l
+ansr = r
+while l<r:
+    sumdata = data[l] + data[r]
+    if abs(sumdata)<answer:
+        answer = abs(sumdata)
+        ansl = l
+        ansr = r
+
+    if sumdata > 0:
+        r -= 1
+    else:
+        l += 1
+
+print(data[ansl], data[ansr])n = int(input())
