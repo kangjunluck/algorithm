2df603ddd4eeb2edf9ea2aee44e54fb7cc6cc51b
DESKTOP-KVCQHCD\multicampus <kangjunlucky@gmail.com>
2021-08-29 11:18:34 +0900

    algo upload

--- 문자열/A와B.py
+++ 문자열/A와B.py
@@ -5,6 +5,13 @@ input = sys.stdin.readline
 a = input().strip()
 b = input().strip()
 
+# aabbbaabaaa
+# i         j
+# bcnt = 0
+# bcnt += 1
+# 만나는 b의 갯수 홀수 i 움직이고 짝수이면 j가 움직인다
+# 길이 같아지는데 b의 갯수가 홀수 뒤집고
+# 구현
 
 
 while len(b) != len(a):
