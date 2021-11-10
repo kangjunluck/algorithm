ce93e2cbc857d59de7e4abd5416c8323f7dc6621
DESKTOP-KVCQHCD\multicampus <kangjunlucky@gmail.com>
2021-11-10 23:37:54 +0900

    1110 algo upload

---
+++ 그래프/파이프옮기기.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+map = [list(map(int, input().split())) for _ in range(n)]
+route = [[(0, 1), (1, 1)], [(0, 1),(1, 1), (1, 0)], [(1, 1), (1, 0)]]
+answer = 0
+
+def dfs(a, b, c):
+    global answer
+    if a == n-1 and b == n-1:
+        answer += 1
+        return
+    for ro in route[c]:
+        nr = a + ro[0]
+        nc = b + ro[1]
+        if ro == (1, 1):
+            if 0<=nr<n and 0<=nc<n and map[nr][nc] == 0 and map[nr][nc-1] == 0 and map[nr-1][nc] == 0:
+                dfs(nr, nc, 1)
+        else:
+            if 0<=nr<n and 0<=nc<n and map[nr][nc] == 0:
+                if ro == (0, 1):
+                    dfs(nr, nc, 0)
+                else:
+                    dfs(nr, nc, 2)
+
+dfs(0, 1, 0)
+print(answer)
